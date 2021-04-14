73f9daefe9db047a19c77b78e7390d7be292f9b7
PI2 <mathias.v.andersen@gmail.com>
2021-04-14 15:48:07 +0200

    testet

--- cppkode/readFPGA/Main.cpp
+++ cppkode/readFPGA/Main.cpp
@@ -37,13 +37,6 @@ void TESTreadFPGA(){
 	}
 }
 
-
-
-int a[7];
-void readFPGA(){
-	
-}
-
 const int pin0 = 0;
 const int pin1 = 2;
 const int pin2 = 3;
@@ -52,11 +45,9 @@ const int pin4 = 4;
 const int pin5 = 5;
 const int pin6 = 6;
 const int pin7 = 26;
-
-const int mikrosekund = 1000000;
-
-int main(){
-	wiringPiSetup();
+unsigned int number = 0b00000000;
+int a[7];
+void readFPGA(){
 	pinMode(pin0, INPUT);
 	pinMode(pin1, INPUT);
 	pinMode(pin2, INPUT);
@@ -65,39 +56,77 @@ int main(){
 	pinMode(pin5, INPUT);
 	pinMode(pin6, INPUT);
 	pinMode(pin7, INPUT);
+	a[0] = digitalRead(pin0);
+	a[1] = digitalRead(pin1);
+	a[2] = digitalRead(pin2);
+	a[3] = digitalRead(pin3);
+	a[4] = digitalRead(pin4);
+	a[5] = digitalRead(pin5);
+	a[6] = digitalRead(pin6);
+	a[7] = digitalRead(pin7);
+	/*cout << pin0 << " :" << a[0] << "\n";
+	cout << pin1 << " :" << a[1] << "\n";
+	cout << pin2 << " :" << a[2] << "\n";
+	cout << pin3 << " :" << a[3] << "\n";
+	cout << pin4 << " :" << a[4] << "\n";
+	cout << pin5 << " :" << a[5] << "\n";
+	cout << pin6 << " :" << a[6] << "\n";
+	cout << pin7 << " :" << a[7] << "\n \n";
+	cout << "\n";*/
+	number = 0b00000000;
+	for (int i=0; i<8; i++){
+		number += a[i] << i;
+	}
+}
+
+
+
+const int mikrosekund = 1000000;
+int count = 0;
+int main(){
+	wiringPiSetup();
 	
 	
 	
 	
 	while(1){
-		cout << pin0 << " :" << digitalRead(pin0) << "\n";
-		cout << pin1 << " :" << digitalRead(pin1) << "\n";
-		cout << pin2 << " :" << digitalRead(pin2) << "\n";
-		cout << pin3 << " :" << digitalRead(pin3) << "\n";
-		cout << pin4 << " :" << digitalRead(pin4) << "\n";
-		cout << pin5 << " :" << digitalRead(pin5) << "\n";
-		cout << pin6 << " :" << digitalRead(pin6) << "\n";
-		cout << pin7 << " :" << digitalRead(pin7) << "\n \n";
-		cout << "\n";
-		usleep(0.2*mikrosekund);
-		a[0] = digitalRead(pin0);
-		a[1] = digitalRead(pin1);
-		a[2] = digitalRead(pin2);
-		a[3] = digitalRead(pin3);
-		a[4] = digitalRead(pin4);
-		a[5] = digitalRead(pin5);
-		a[6] = digitalRead(pin6);
-		a[7] = digitalRead(pin7);
 		
-		unsigned int number = 0b00000000;
-		for (int i=0; i<8; i++){
-			number += a[i] << i;
+		
+		
+		for (int i=0; i<m; i++){
+			for(int j=0; j<n; j++){
+				readFPGA();
+				if(count = 0){
+					image[i][j].R = number;
+					count++;
+					//cout << number << "\n";
+				}
+			}
+			for(int j=0; j<n; j++){
+				readFPGA();
+				if(count = 1){	
+					image[i][j].G = number;
+					count++;
+					//cout << number << "\n";
+				}
+			}
+			for(int j=0; j<n; j++){
+				readFPGA();
+				if(count =2){	
+					image[i][j].B = number;
+					count = 0;
+					//cout << number << "\n";
+				}
+			}
 		}
 		
+		cout << "R: " << image[2][2].R << "\n";
+		cout << "G: " << image[2][2].G << "\n";
+		cout << "B: " << image[235][2].B << "\n";
 		//number = a[7] << 7 | a[6] << 6 ;
 		
-		cout << number << "\n";
-		usleep(5*mikrosekund);
+		//cout << number << "\n";
+		usleep(0.5*mikrosekund);
 		
 	}
 }
