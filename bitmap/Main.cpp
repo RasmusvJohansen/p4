10ad691949e229bd1189b1d6a144b82705786f4e
viborgen <mathias.v.andersen@gmail.com>
2021-05-04 15:50:58 +0200

    bitmap

--- bitmap/Main.cpp
+++ bitmap/Main.cpp
@@ -10,11 +10,10 @@ using namespace std;
 #include "Image.h"  
 
 
-
 const int mikrosekund = 1000000;
 //Billede størrelse
-const int m = 640;
-const int n = 480;
+const int m = 20;
+const int n = 20;
 
 //Struct til at gemme RGB data for hver pixel
 struct pixel{
@@ -78,14 +77,37 @@ void readFPGA(){
 	}
 }
 
-
+int array[m*n]={255,128,255,128,255,128,247,128,139,115,19,140,0,96,0,153,71,96,143,126,255,128,255,128,255,128,247,128,138,115,19,140,0,96,0,153,71,96,143,126,255,128,255,128,255,128,247,128,139,115,19,140,0,95,0,153,71,95,143,127,255,128,255,128,255,128,247,128,139,115,19,140,0,95,0,153,71,95,143,127,255,128,255,128,255,128,247,128,139,115,19,140,0,96,0,153,72,96,143,126,255,128,255,128,255,128,246,128,139,115,19,140,0,95,0,153,72,95,143,132,255,128,255,128,255,128,246,128,139,115,19,140,0,95,0,153,71,95,143,127,255,128,255,128,255,128,246,128,139,115,19,140,0,95,0,153,71,95,143,128,255,128,255,128,255,128,246,128,139,115,19,140,0,95,0,153,71,95,143,127,255,128,255,128,255,128,246,128,139,115,19,140,0,95,0,153,71,95,143,127,255,128,255,128,255,128,246,128,139,115,19,140,0,95,0,153,72,95,143,131,255,128,255,128,255,128,246,128,139,115,19,140,0,95,0,153,72,95,143,127,255,128,255,128,255,128,246,128,138,115,19,140,0,95,0,153,71,95,143,128,255,128,255,128,255,128,247,128,139,115,19,140,0,95,0,153,71,95,143,128,255,128,255,128,255,128,247,128,139,115,19,140,0,95,0,153,71,95,143,128,255,128,255,128,255,128,246,128,138,115,19,140,0,95,0,153,71,95,143,129,255,128,255,128,255,128,246,128,139,115,19,140,0,95,0,153,71,95,143,125,255,128,255,128,255,128,246,128,139,115,19,140,0,95,0,153,71,95,143,125,255,128,255,128,255,128,247,128,139,115,20,140,0,96,0,154,71,96,143,125};
 
 
 
 int main(){
-	wiringPiSetup();
+	int k = 0;
+//	wiringPiSetup();
+	//cout << "her";
+	for(int i = 0; i<19; i++){
+		for(int j = 0; j<6; j++){
+			billede[i][j].R = array[k];
+			cout << array[k] << "R\n";
+			k++;
+			billede[i][j].G = array[k];
+			cout << array[k] << "G\n";
+			k++;
+			billede[i][j].B = array[k];
+			k++;
+			cout << array[k] << "B\n";
+		}
+		//k=0;
+		cout << i << "den er her  \n";
+	}
+	
+	
+	
+	
+	
+	
 		
-		int count = 0;
+	/*	int count = 0;
 		
 		for(int i=0; i<m;i++){
 			for(int j=0; j<n;j++){
@@ -110,7 +132,7 @@ int main(){
 					//usleep(0.00001);
 				}
 			}
-		}
+		}*/
 		/*cout << "R: " << billede[2][2].R << "\n";
 		cout << "G: " << billede[2][2].G << "\n";
 		cout << "B: " << billede[2][2].B << "\n";
@@ -121,20 +143,23 @@ int main(){
 		cout << "G: " << billede[400][400].G << "\n";
 		cout << "B: " << billede[400][400].B << "\n"; */
 	
-    const int width = 640;
-    const int height = 480;
+    const int width = 19;
+    const int height = 19;
 
     Image image(width, height);
-	/*
-    for (int y= 0; y < height; y++)
+	
+    for (int x= 0; x < width; x++)
     {
-        for (int x = 0; x < width; x++)
+        for (int y = 0; y < width; y++)
         {
-			image.SetColor(Color(billede[height][width].R,billede[height][width].G,billede[height][width].B), x, y);
+			image.SetColor(Color(billede[x][y].R,billede[x][y].G,billede[x][y].B), x, y);
+			//cout << billede[x][y].R << "\n";
+			//cout << billede[x][y].G << "\n";
+			//cout << billede[x][y].B << "\n";
         }
     }
-    * */
-    std::chrono::steady_clock::time_point _start(std::chrono::steady_clock::now());
+    image.Export("arduino.bmp");
+    /*std::chrono::steady_clock::time_point _start(std::chrono::steady_clock::now());
     for (int y= 0; y < height; y++)
     {
         for (int x = 0; x < width; x++)
@@ -145,7 +170,7 @@ int main(){
     image.Export("image10.bmp");
 	std::chrono::steady_clock::time_point _end(std::chrono::steady_clock::now());
 	std::cout << std::chrono::duration_cast<std::chrono::duration<double>>(_end - _start).count();
-	cout << "\n";
+	cout << "\n";*/
 
 
 }
