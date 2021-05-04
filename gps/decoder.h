5123eb805412b9f8b0d916a28d26d79ce67564fc
viborgen <mathias.v.andersen@gmail.com>
2021-05-04 11:16:31 +0200

    gps virker

--- gps/decoder.h
+++ gps/decoder.h
@@ -14,9 +14,9 @@ string lonn;
 string lo;
 string height;
 bool pData = false;
-float lattf;
-float lonnf;
-float heightf;
+double lattf;
+double lonnf;
+double heightf;
 
 
 void NMEA_decoder2(string data){  // ny decoder. kan tage højde for komma tal
@@ -24,10 +24,6 @@ void NMEA_decoder2(string data){  // ny decoder. kan tage højde for komma tal
   int k = 0;
   string dataArray[14]; // er 14 da sætnings konstruktionen på et gps signal har 15 sætninger
   //String tmp[85]; // er 85 stor da et gps signal maks har 85 char
-
-  /*for ( int i = 0 ; i <=85 ; i++){
-    tmp[i] = data.substring(i, i+1);  // for loop der deler hver char op i et array, for kunne tjekke for "end bit" senere
-  }*/  // fandt ud af dette ikke var nødvendigt
   
   if (data.substr(0,6) == "$GPGGA"){ // tjekker om det er positions data
   for (int i = 0 ; data[i] != '*' ; i++){ //for loop der køre indtil '' som er end bit 
@@ -40,17 +36,19 @@ void NMEA_decoder2(string data){  // ny decoder. kan tage højde for komma tal
       k++; // k tæller antal sætning der er indeholdet    //i holder hvor mange char der er talt i alt. j bliver sat til 0 igen så den tæller hvor mange chars der imellem to kommaer
     }
   }
-  cout << dataArray[2] << "\n";
-  float floatDataArray[14];
-  floatDataArray[0] = stof(dataArray[2].substr(0,2));
-  floatDataArray[1] = stof(dataArray[2].substr(2,8))/60;
-  floatDataArray[2] = stof(dataArray[2].substr(13,3));
-  floatDataArray[3] = stof(dataArray[2].substr(16,8))/60;
-  float lat = floatDataArray[0] + floatDataArray[1];
-  float lon = floatDataArray[2] + floatDataArray[3];
-  cout << lat << "\n";
-  cout << lon << "\n";
+ 
+  double floatDataArray[14]; //benyttes til at konvertere dataen til floats (nu doubles).
+  floatDataArray[0] = stod(dataArray[2].substr(0,2));
+  floatDataArray[1] = stod(dataArray[2].substr(2,8))/60.0;
+  floatDataArray[2] = stod(dataArray[2].substr(13,3));
+  floatDataArray[3] = stod(dataArray[2].substr(16,8))/60.0;
+  double lat = floatDataArray[0] + floatDataArray[1];
+  double lon = floatDataArray[2] + floatDataArray[3];
+  
+  cout << "\n" << setprecision(9) << lat << "\n";
+  cout << setprecision(9) << lon << "\n";
   height = dataArray[9]+dataArray[10]; //--//--
+  
   pData = true; // boolean der sørger for overstående data kun bliver printet én gang i void loop
  }
 }
--- gps/test.cpp
+++ gps/test.cpp
@@ -15,68 +15,12 @@
 using namespace std;
 #include <sstream>
 #include <iomanip>
-//#include "decoder.h"
+#include "decoder.h"
 
 int i;
 
-string gpsDATA;
-string latt;
-string la;
-string lonn;
-string lo;
-string height;
-bool pData = false;
-double lattf;
-double lonnf;
-double heightf;
-
-
-void NMEA_decoder2(string data){  // ny decoder. kan tage højde for komma tal
-  int j = 0;
-  int k = 0;
-  string dataArray[14]; // er 14 da sætnings konstruktionen på et gps signal har 15 sætninger
-  //String tmp[85]; // er 85 stor da et gps signal maks har 85 char
-
-  /*for ( int i = 0 ; i <=85 ; i++){
-    tmp[i] = data.substring(i, i+1);  // for loop der deler hver char op i et array, for kunne tjekke for "end bit" senere
-  }*/  // fandt ud af dette ikke var nødvendigt
-  
-  if (data.substr(0,6) == "$GPGGA"){ // tjekker om det er positions data
-	  cout << "godkendt\n";
-  for (int i = 0 ; data[i] != '*' ; i++){ //for loop der køre indtil '' som er end bit 
-    if(data[i] !=','){ // denne if sætning tæller hvor mange chars der er til næste komma
-      j++;
-    }
-    else{
-      dataArray[k] = data.substr(i-j,i);               // data bliver indsæt i array som er delt op i sætninger af k. 
-      j = 0;                                              //Sætninger bliver definere ved i og j, hvor j har talt hvor mange char der er til et komma. 
-      k++; // k tæller antal sætning der er indeholdet    //i holder hvor mange char der er talt i alt. j bliver sat til 0 igen så den tæller hvor mange chars der imellem to kommaer
-    }
-  }
-  // << dataArray[13] << "   her\n";
-  
-  for(int i;i<15;i++){
-  //cout << dataArray[i] << "   her\n";
-  }
-  double floatDataArray[14];
-  floatDataArray[0] = stod(dataArray[2].substr(0,2));
-  floatDataArray[1] = stod(dataArray[2].substr(2,8))/60.0;
-  floatDataArray[2] = stod(dataArray[2].substr(13,3));
-  floatDataArray[3] = stod(dataArray[2].substr(16,8))/60.0;
-  
-  double lat = floatDataArray[0] + floatDataArray[1];
-  double lon = floatDataArray[2] + floatDataArray[3];
-  cout << setprecision(9) << lat << "\n";
-  cout << setprecision(9) << lon << "\n";
-  height = dataArray[9]+dataArray[10]; //--//--
-  pData = true; // boolean der sørger for overstående data kun bliver printet én gang i void loop
- }
-}
-
-
-int main ()
-{
-  int serial_port; 
+void laesGPS(){
+	  int serial_port; 
   char dat,buff[100],GGA_code[3];
   unsigned char IsitGGAstring=0;
   unsigned char GGA_index=0;
@@ -85,21 +29,20 @@ int main ()
   if ((serial_port = serialOpen ("/dev/ttyS0", 9600)) < 0)		/* open serial port */
   {
     fprintf (stderr, "Unable to open serial device: %s\n", strerror (errno)) ;
-    return 1 ;
+    //return 1 ;
   }
 
   if (wiringPiSetup () == -1)							/* initializes wiringPi setup */
   {
     fprintf (stdout, "Unable to start wiringPi: %s\n", strerror (errno)) ;
-    return 1 ;
+    //return 1 ;
   }
 string str;
 ofstream myfile;
-string testArray[67];
+string gpsArray[67];
 stringstream ss;
-int l = 0;
+	int l = 0;
   while(1){
-			
 		if(serialDataAvail (serial_port) )		/* check for any data available on serial port */
 		  { 
 			dat = serialGetchar(serial_port);		/* receive character serially */		
@@ -109,7 +52,7 @@ int l = 0;
 			}
 			else if(IsitGGAstring ==1){
 				//cout << dat;
-				testArray[l] = dat;
+				gpsArray[l] = dat;
 				l++;
 				//buff[GGA_index++] = dat;
 				if(dat=='\r')
@@ -129,13 +72,11 @@ int l = 0;
 		  }
 		if(is_GGA_received_completely==1){
 			//printf("$GPGGA,%s",buff);
-			l = 0;	
+			l = 0;	//array resettes
 			ss.str("");
 			ss.clear();
-			for(int i = 0; i<67; i++){
-				//cout << testArray[i];
-				ss << testArray[i];
-				//ss >> str;
+			for(int i = 0; i<67; i++){ //laver datarray om til en string, så den kan  bruges i decoderen.
+				ss << gpsArray[i];
 			}
 			string test;
 			string strr("$GPGGA,"+ss.str());
@@ -165,6 +106,15 @@ int l = 0;
 				
 		}
 	}
+}
+
+
+
+
+
+int main ()
+{
+	laesGPS();
 	return 0;
 }
 
